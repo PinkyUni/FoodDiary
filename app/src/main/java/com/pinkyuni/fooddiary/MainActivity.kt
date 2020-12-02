997889ee46da9c3936487fb33d53b0d642f0d427
PinkyUni <30stmars6277@gmail.com>
2020-12-02 11:23:45 +0300

    retrieve food vitamins

--- app/src/main/java/com/pinkyuni/fooddiary/MainActivity.kt
+++ app/src/main/java/com/pinkyuni/fooddiary/MainActivity.kt
@@ -3,12 +3,7 @@ package com.pinkyuni.fooddiary
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.pinkyuni.fooddiary.entities.associative.FoodVitaminCrossRef
-import com.pinkyuni.fooddiary.entities.core.Food
-import com.pinkyuni.fooddiary.entities.core.Gender
-import com.pinkyuni.fooddiary.entities.core.Vitamin
 import com.pinkyuni.fooddiary.data.DiaryDatabase
-import com.pinkyuni.fooddiary.usecases.GenderDao
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -17,7 +12,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private var db: DiaryDatabase? = null
-    private var genderDao: GenderDao? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,17 +36,4 @@ class MainActivity : AppCompatActivity() {
             }.subscribe()
     }
 
-    private fun initGender() {
-        Observable.fromCallable {
-            db = DiaryDatabase.getAppDataBase(context = this)
-            genderDao = db?.genderDao()
-            genderDao?.getGenders()
-        }.subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .doOnNext { list ->
-                var finalString = ""
-                list?.map { finalString += it.name + " - " }
-                tvMain.text = finalString
-            }.subscribe()
-    }
 }
