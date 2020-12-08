5a66519e065dfad56b45effc09f5e1ce45451601
PinkyUni <30stmars6277@gmail.com>
2020-12-09 00:32:52 +0300

    update ui

--- app/src/main/java/com/pinkyuni/fooddiary/ui/MainActivity.kt
+++ app/src/main/java/com/pinkyuni/fooddiary/ui/MainActivity.kt
@@ -3,26 +3,27 @@ package com.pinkyuni.fooddiary.ui
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.pinkyuni.fooddiary.R
 import com.pinkyuni.fooddiary.data.model.FoodInfo
+import com.pinkyuni.fooddiary.databinding.ActivityMainBinding
 import com.pinkyuni.fooddiary.ui.base.ResultObserver
-import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity(), ResultObserver<FoodInfo> {
 
+    private lateinit var binding: ActivityMainBinding
     private val viewModel by viewModel<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel.getFoodInfo(4)
         viewModel.result.observe(this, this)
     }
 
     override fun onDataLoaded(data: FoodInfo) {
-        tvMain.text = data.toString()
+        binding.tvMain.text = data.toString()
     }
 
     override fun onTaskComplete() {
@@ -30,7 +31,7 @@ class MainActivity : AppCompatActivity(), ResultObserver<FoodInfo> {
     }
 
     override fun onError(message: String) {
-        tvMain.text = "Error occurred :/\nTry again later"
+        binding.tvMain.text = "Error occurred :/\nTry again later"
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
